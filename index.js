3df951e6fada2b870a02eb36ebe7be8dd4ed79f5
Walter Simon <simonwalter986@gmail.com>
2022-06-18 01:59:58 +0300

    commit tree 62d4b685719bd9be56f54c09aae4527b09b43655

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers() {
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers() {
+    return drivers.slice(-2);
+}
+
